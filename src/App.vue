2d8d7b5cc41952d973522a4e3674a5cdfac44b35
Manuel Rivera <menu.rivera@gmail.com>
2022-01-14 16:10:21 -0600

    Progress made

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,37 @@
 <template>
   <h1 class="mt-5 ms-5">Pidencio / levantar pedido</h1>
-  <div class="ms-5 mb-5">
-    <form class="d-flex justify-content-evenly" @submit="enviarOrden">
+  <div class="ms-5 mb-5 me-5">
+    <form class="d-flex justify-content-evenly" @submit.prevent="agregarOrdenALista">
 
       <div class="flex-grow-1">
-        <!-- TODO: Agregar la posibilidad de agregar más puntos de recolección -->
-        <div class="mt-3">
-          <!-- NOTE: recoleccion -->
-          <h3>Datos del pedido <strong>(Recolección)</strong></h3>
+        <!-- NOTE: recolecciones -->
+        <button class="btn btn-warning" v-on:click.prevent="agregarRecoleccion">Agregar recolección</button>
+        <div class="mt-3" v-for="recoleccion, index in pedido.recolecciones" :key="index">
+          <h3>Datos del pedido <strong>(Recolección {{index + 1}})</strong></h3>
+          <button v-if="index > 0" class="btn btn-danger" v-on:click.prevent="eliminarRecoleccion(index)">Eliminar esta recoleccion</button>
           <div>
-            <label for="recoleccion-nombre">Nombre del destino:</label>
-            <input type="text" id="recoleccion-nombre" class="d-block" v-model="recoleccion.nombre" placeholder="EJ. nombre del restaurante" required>
+            <label :for="`recoleccion${index}-nombre`">Nombre del destino:</label>
+            <input type="text" :id="`recoleccion${index}-nombre`" class="d-block" v-model="pedido.recolecciones[index].nombre" placeholder="EJ. nombre del restaurante" required>
           </div>
 
           <div>
             <label for="orden">Orden:</label>
-            <textarea id="orden" class="d-block" v-model="recoleccion.orden" required> </textarea>
+            <textarea id="orden" class="d-block" v-model="pedido.recolecciones[index].orden" required> </textarea>
           </div>
 
           <div>
-            <label for="recoleccion-domicilio">Domicilio:</label>
-            <input type="text" id="recoleccion-domicilio" class="d-block" v-model="recoleccion.domicilio" placeholder="Calle, número y colonia" required>
+            <label :for="`recoleccion${index}-domicilio`">Domicilio:</label>
+            <input type="text" :id="`recoleccion${index}-domicilio`" class="d-block" v-model="pedido.recolecciones[index].domicilio" placeholder="Calle, número y colonia" required>
           </div>
 
           <div>
-            <label for="recoleccion-referencias">Referencias del domicilio:</label>
-            <textarea id="recoleccion-referencias" class="d-block" v-model="recoleccion.referencias" placeholder="Cruce, fachada, ubicación aprox" required> </textarea>
+            <label :for="`recoleccion${index}-referencias`">Referencias del domicilio:</label>
+            <textarea :id="`recoleccion${index}-referencias`" class="d-block" v-model="pedido.recolecciones[index].referencias" placeholder="Cruce, fachada, ubicación aprox" required> </textarea>
           </div>
 
           <div>
-            <label for="recoleccion-indicaciones">Indicaciones especiales del recoleccion:</label>
-            <textarea id="recoleccion-indicaciones" class="d-block" v-model="recoleccion.indicacionesEspeciales" required> </textarea>
+            <label :for="`recoleccion${index}-indicaciones`">Indicaciones especiales del recoleccion:</label>
+            <textarea :id="`recoleccion${index}-indicaciones`" class="d-block" v-model="pedido.recolecciones[index].indicacionesEspeciales" required> </textarea>
           </div>
         </div>
       </div>
@@ -41,22 +42,22 @@
           <h3>Datos del cliente <strong>(Entrega)</strong></h3>
           <div>
             <label for="nombre">Nombre del cliente:</label>
-            <input type="text" id="nombre" class="d-block" v-model="cliente.nombre" required>
+            <input type="text" id="nombre" class="d-block" v-model="pedido.entrega.nombre" required>
           </div>
 
           <div>
             <label for="numero">Télefono:</label>
-            <input type="number" id="numero" class="d-block" v-model="cliente.numero" required>
+            <input type="number" id="numero" class="d-block" v-model="pedido.entrega.numero" required>
           </div>
 
           <div>
             <label for="domicilio">Domicilio:</label>
-            <input type="text" id="domicilio" class="d-block" v-model="cliente.domicilio" placeholder="Calle, número y colonia" required>
+            <input type="text" id="domicilio" class="d-block" v-model="pedido.entrega.domicilio" placeholder="Calle, número y colonia" required>
           </div>
 
           <div>
             <label for="referencias">Referencias del domicilio:</label>
-            <textarea id="referencias" class="d-block" v-model="cliente.referencias" placeholder="Cruce, fachada, ubicación aprox" required> </textarea>
+            <textarea id="referencias" class="d-block" v-model="pedido.entrega.referencias" placeholder="Cruce, fachada, ubicación aprox" required> </textarea>
           </div>
         </div>
 
@@ -65,30 +66,38 @@
           <h3>Costo de envío:</h3>
           <div>
             <label for="costo">Total:</label>
-            <input type="number" class="d-block" id="costo" v-model="costo" required>
+            <input type="number" class="d-block" id="costo" v-model="pedido.costo" required>
           </div>
         </div>
 
-        <div class="mt-3">
-          <!-- NOTE: repartidor -->
+        <!-- <div class="mt-3">
           <h3>Asignar al repartidor:</h3>
 
           <div v-for="repartidor, index, in repartidores" :key="index">
             <input type="radio" :id="`${repartidor.telefono}-${index}`" class="me-2" :value="repartidor" v-model="rep">
             <label :for="`${repartidor.telefono}-${index}`">{{repartidor.nombre}}</label>
           </div>
-        </div>
+        </div> -->
 
-        <!-- NOTE: Ordenar -->
         <div class="mt-3">
-          <button v-if="rep" type="submit" class="btn btn-primary">Enviar orden a {{rep.nombre}}</button>
-          <p v-else class="text-danger">Selecciona un repartidor para continuar</p>
+          <!-- NOTE: Ordenar -->
+          <button type="submit" class="btn btn-primary p-3">Agregar orden a lista de espera</button>
         </div>
       </div>
 
-      <div class="flex-grow-1">
+      <div id="lista-espera" class="flex-grow-1 ps-3">
+        <!-- NOTE: Lista de espera -->
         <h3>Lista de espera</h3>
-
+        <div v-for="pedidoListo, index in lista" :key="index" class="bg-grey p-2 rounded mb-4">
+          <p>Para: <strong>{{pedidoListo.entrega.nombre}}</strong></p>
+          <p>Costo de envio: <strong>${{pedidoListo.costo}}</strong></p>
+          <p>Recolecciones:</p>
+          <div class="ms-4">
+            <div v-for="recoleccion, index2 in pedidoListo.recolecciones" :key="index2">
+              <p>{{index2 + 1}}: <strong>{{recoleccion.nombre}}</strong></p>
+            </div>
+          </div>
+        </div>
       </div>
 
     </form>
@@ -97,84 +106,80 @@
 </template>
 
 <script>
-import { repartidores } from "./util/repartidores";
 import "bootstrap/dist/css/bootstrap.css";
+import { repartidores } from "./util/repartidores";
+import { recoleccionSchema } from "./util/recoleccionSchema";
 import { reactive, ref } from "@vue/reactivity";
 export default {
   name: "App",
   setup() {
-    console.log(repartidores);
-    const cliente = reactive({
-      nombre: "",
-      numero: undefined,
-      domicilio: "",
-      referencias: "",
+    // Setup state
+    var lista = ref([]);
+    const pedido = reactive({
+      entrega: {
+        nombre: "",
+        numero: undefined,
+        domicilio: "",
+        referencias: "",
+      },
+      recolecciones: [
+        {
+          ...recoleccionSchema,
+        },
+      ],
+      costo: 38,
     });
 
-    const recoleccion = reactive({
-      nombre: "",
-      orden: "",
-      domicilio: "",
-      referencias: "",
-      indicacionesEspeciales: "",
-    });
+    // Setup local storage
+    const rawLista = JSON.parse(localStorage.getItem("lista"));
+    if (!rawLista) {
+      // set default values to lista
+      console.log("Configurando local storage");
+      localStorage.setItem("lista", JSON.stringify([]));
+    }
+    lista.value = JSON.parse(localStorage.getItem("lista"));
+    console.log("Lista de pedidos:", lista.value);
 
-    const rep = ref("");
-    const costo = ref(38);
-
-    const enviarOrden = () => {
-      // Armar la orden
-      var entrega = `*PEDIDO PARA: ${cliente.nombre.toUpperCase()}*\n  Teléfono: ${
-        cliente.numero
-      }\n  Domicilio: ${cliente.domicilio}\n  Referencias: \n${
-        cliente.referencias
-      }\n`;
-      var recoleccionString = `*RECOLECCIÓN: ${recoleccion.nombre}*\n  Domicilio: ${recoleccion.domicilio}\n  Referencias: \n${recoleccion.referencias}\n  Orden: \n${recoleccion.orden}\n  Indicaciones especiales: ${recoleccion.indicacionesEspeciales}\n`;
-
-      var totalEnvio = `*COSTO DE ENVÍO: $${costo.value}*`;
-
-      var orden = `${entrega}\n${recoleccionString}\n${totalEnvio}`;
-
-      // redirigir al usuario al link
-      const ordenUrl = `https://api.whatsapp.com/send/?phone=52${
-        rep.value.telefono
-      }&text=${encodeURIComponent(orden)}&app_absent=0`;
-      window.open(ordenUrl, "https://web.whatsapp.com/");
-
-      // limpiar las forms
-      cliente.nombre = "";
-      cliente.numero = undefined;
-      cliente.domicilio = "";
-      cliente.referencias = "";
-      recoleccion.nombre = "";
-      recoleccion.orden = "";
-      recoleccion.domicilio = "";
-      recoleccion.referencias = "";
-      recoleccion.indicacionesEspeciales = "";
-      rep.value = "";
+    // Functions
+    const agregarRecoleccion = () => {
+      pedido.recolecciones.push({ ...recoleccionSchema });
     };
 
+    const eliminarRecoleccion = (index) => {
+      pedido.recolecciones.splice(index, 1);
+    };
+
+    const agregarOrdenALista = () => {
+      // Agregar el pedido a la lista
+      lista.value.push(pedido);
+
+      // Guardar la lista en el localStorage
+      localStorage.setItem("lista", JSON.stringify(lista.value));
+
+      // // limpiar las forms
+    };
+
+    // Return to bind 'em
     return {
-      cliente,
-      recoleccion,
+      pedido,
+      agregarRecoleccion,
+      eliminarRecoleccion,
+      agregarOrdenALista,
       repartidores,
-      rep,
-      costo,
-      enviarOrden,
+      lista,
     };
   },
 };
 </script>
 
 <style>
-.btn-primary {
-  width: 300px;
-  height: 70px;
+p {
+  margin: 0;
 }
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-} */
+#lista-espera {
+  border-left: 1px solid rgb(126, 126, 126);
+}
+.bg-grey {
+  background-color: rgb(219, 219, 219);
+}
 </style>
--- src/components/listaDeEspera.vue
+++ src/components/listaDeEspera.vue
@@ -1,23 +1,36 @@
 <template>
   <div v-for="pedido, index in pedidos" :key="index">
-      <p>{{pedido.cliente.nombre}}</p>
+    <p>{{pedido.cliente.nombre}}</p>
   </div>
 </template>
 
 <script>
 export default {
-    name: "lista",
-    setup(props) {
-        const {pedidos} = props;
+  name: "lista",
+  setup(props) {
+    const { pedidos } = props;
+    // // Armar la orden
+    // lista.value.push(JSON.parse(JSON.stringify(pedido)));
+    // var entrega = `*PEDIDO PARA: ${cliente.nombre.toUpperCase()}*\n  Teléfono: ${
+    //   cliente.numero
+    // }\n  Domicilio: ${cliente.domicilio}\n  Referencias: \n${
+    //   cliente.referencias
+    // }\n`;
+    // var recoleccionString = `*RECOLECCIÓN: ${recoleccion.nombre}*\n  Domicilio: ${recoleccion.domicilio}\n  Referencias: \n${recoleccion.referencias}\n  Orden: \n${recoleccion.orden}\n  Indicaciones especiales: ${recoleccion.indicacionesEspeciales}\n`;
+    // var totalEnvio = `*COSTO DE ENVÍO: $${costo.value}*`;
+    // var orden = `${entrega}\n${recoleccionString}\n${totalEnvio}`;
+    // // redirigir al usuario al link
+    // const ordenUrl = `https://api.whatsapp.com/send/?phone=52${
+    //   rep.value.telefono
+    // }&text=${encodeURIComponent(orden)}&app_absent=0`;
+    // window.open(ordenUrl, "https://web.whatsapp.com/");
 
-        return {
-            pedidos
-        }
-    }
-
-}
+    return {
+      pedidos,
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>

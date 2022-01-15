331377573439514877051e4df60f7cbba2a8b276
Manuel Rivera <menu.rivera@gmail.com>
2022-01-15 01:26:42 -0600

    api almost created

--- src/js/asignarPedido.js
+++ src/js/asignarPedido.js
@@ -3,12 +3,13 @@ export const asignarPedido = (pedido, repartidor) => {
     repartidor = JSON.parse(JSON.stringify(repartidor))
     const separador = '\n---------------------------------------------------------------\n\n'
 
+    var domicilios = '';
 
     // Codificar el pedido
     var entrega = '';
-    entrega += `PEDIDO PARA: *${pedido.entrega.nombre.toUpperCase()}*\n`
-    entrega += `    Teléfono: ${pedido.entrega.numero}\n`
-    entrega += `    Domicilio: ${pedido.entrega.domicilio}\n`
+    entrega += `PEDIDO PARA: *${pedido.entrega.nombre.toUpperCase()}*\n`;
+    entrega += `    Teléfono: ${pedido.entrega.numero}\n`;
+    entrega += `    Domicilio: ${pedido.entrega.domicilio}\n`;
     entrega += `    Referencias: ${pedido.entrega.referencias}\n`;
     entrega += separador;
 
@@ -23,11 +24,17 @@ export const asignarPedido = (pedido, repartidor) => {
         recoleccionString += `        Indicaciones especiales: ${recoleccion.indicacionesEspeciales}\n\n`;
         recoleccionString += `        Orden: \n${recoleccion.orden}\n`
         recoleccionString += separador;
+
+        domicilios += encodeURIComponent(recoleccion.domicilio) + '/'
     }
+    domicilios += encodeURIComponent(pedido.entrega.domicilio);
 
     var totalEnvio = `COSTO DE ENVÍO: *$${pedido.costo}*`;
 
-    var orden = `${entrega}${recoleccionString}${totalEnvio}`;
+    var orden = `${entrega}${recoleccionString}${totalEnvio}\n`;
+    // orden += `    Mapa: https://www.google.com/maps/dir//${domicilios}%20tequila%20jalisco%2046400`;
+    orden += `    Mapa: https://www.google.com/maps/dir//${domicilios}%20tequila%20jalisco%2046400&&?api=1&travelmode=driving`;
+
 
     // Generar la url de whatsapp
     const ordenUrl = `https://api.whatsapp.com/send/?phone=52${repartidor.telefono}&text=${encodeURIComponent(orden)}&app_absent=0`;
